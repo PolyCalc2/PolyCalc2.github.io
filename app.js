7b0f2ca438d2c6f0280093b86de250a6dc91fe7b
Anonymous <58795111+PolyCalc2@users.noreply.github.com>
2019-12-13 18:44:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+//calculates the attack force of a unit
+function attackForce (attack, attackerHealth, attackerMaxHealth) {
+    var finalAttackForce = attack * (attackerHealth / attackerMaxHealth)
+}
+document.getElementById("attackForce").innerHTML = attackForce(2, 10, 10)
 
+//calculates the defense force of a unit
+function defenseForce (defense, defenderHealth, defenderMaxHealth, defenseBonus) {
+    var finalDefenseForce = defense * (defenderHealth / defenderMaxHealth) * defenseBonus
+}
+document.getElementById("defenseForce").innerHTML = defenseForce(1, 10, 10, 1.5)
+
+//calculates the total damage of the attack
+function totalDamage (attackForce, defenseForce) {
+    var finalAttackForce = attack * (attackerHealth / attackerMaxHealth)
+}
+document.getElementById("totalDamage").innerHTML = totalDamage(2, 10, 10)
+
+//calculates the health of the unit being attacked
+function attackResult (attack, attackerHealth, attackerMaxHealth) {
+    var finalAttackForce = attack * (attackerHealth / attackerMaxHealth)
+}
+document.getElementById("attackForce").innerHTML = attackForce(2, 10, 10)
