b49872c11d68d39d42d62a570ffc5efb603085ea
Anonymous <58795111+PolyCalc2@users.noreply.github.com>
2019-12-13 23:18:22 -0500

    Midjiwan's Attack Interactions to JS
    These functions are the base of any interaction in Polytopia.

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 //calculates the attack force of a unit
 function attackForce (attack, attackerHealth, attackerMaxHealth) {
     var finalAttackForce = attack * (attackerHealth / attackerMaxHealth)
+
+    return finalAttackForce
 }
 document.getElementById("attackForce").innerHTML = attackForce(2, 10, 10)
 
 //calculates the defense force of a unit
 function defenseForce (defense, defenderHealth, defenderMaxHealth, defenseBonus) {
     var finalDefenseForce = defense * (defenderHealth / defenderMaxHealth) * defenseBonus
+
+    return finalDefenseForce;
 }
 document.getElementById("defenseForce").innerHTML = defenseForce(1, 10, 10, 1.5)
 
 //calculates the total damage of the attack
 function totalDamage (attackForce, defenseForce) {
-    var finalAttackForce = attack * (attackerHealth / attackerMaxHealth)
+    var finalTotalDamage = attackForce + defenseForce
+
+    return finalTotalDamage;
 }
-document.getElementById("totalDamage").innerHTML = totalDamage(2, 10, 10)
+document.getElementById("totalDamage").innerHTML = totalDamage(attackForce(2, 10, 10), defenseForce(1, 10, 10, 1.5))
 
 //calculates the health of the unit being attacked
-function attackResult (attack, attackerHealth, attackerMaxHealth) {
-    var finalAttackForce = attack * (attackerHealth / attackerMaxHealth)
+function attackResult (attackForce, totalDamage, attack, accelerator) {
+    var finalAttackResult = Math.round((attackForce / totalDamage)) * (attack * accelerator)
+
+    return finalAttackResult;
 }
-document.getElementById("attackForce").innerHTML = attackForce(2, 10, 10)
+document.getElementById("attackResult").innerHTML = attackResult(attackForce(2, 10, 10), totalDamage(attackForce(2, 10, 10), defenseForce(1, 10, 10, 1.5)), 2, 1)
