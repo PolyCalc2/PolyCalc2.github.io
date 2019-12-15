e9de6f4c43b897ed9dd30f8ab6d34f029b1b98bb
eisthefifthletter <57116436+eisthefifthletter@users.noreply.github.com>
2019-12-14 21:51:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-//calculates attackForce
+// determines the defense bonus
+var standardDefenseBonus = false;
+var cityWall = true;
+
+// calculates attackForce
 function attackForce(attack, attackerHealth, attackerMaxHealth) {
     var finalAttackForce = attack * (attackerHealth / attackerMaxHealth)
 
@@ -6,15 +10,21 @@ function attackForce(attack, attackerHealth, attackerMaxHealth) {
 }
 document.getElementById("attackForce").innerHTML = attackForce(2, 10, 10)
 
-//calculates defenseForce
+// calculates defenseForce
 function defenseForce(defense, defenderHealth, defenderMaxHealth, defenseBonus) {
+    if (standardDefenseBonus) {
+        defenseBonus = 1.5
+    } else if (cityWall) {
+        defenseBonus = 4
+    }
+    
     var finalDefenseForce = defense * (defenderHealth / defenderMaxHealth) * defenseBonus
 
     return finalDefenseForce;
 }
-document.getElementById("defenseForce").innerHTML = defenseForce(1, 10, 10, 1.5)
+document.getElementById("defenseForce").innerHTML = defenseForce(2, 10, 10, 0)
 
-//calculates totalDamage
+// calculates totalDamage
 function totalDamage(attackForce, defenseForce) {
     var finalTotalDamage = attackForce + defenseForce
 
@@ -22,7 +32,7 @@ function totalDamage(attackForce, defenseForce) {
 }
 document.getElementById("totalDamage").innerHTML = totalDamage(attackForce(2, 10, 10), defenseForce(1, 10, 10, 1.5))
 
-//calculates how much damage is dealt by the attacker
+// calculates how much damage is dealt by the attacker
 function attackResult(attackForce, totalDamage, attack, accelerator) {
     var finalAttackResult = Math.round((attackForce / totalDamage)) * (attack * accelerator)
 
@@ -30,9 +40,11 @@ function attackResult(attackForce, totalDamage, attack, accelerator) {
 }
 document.getElementById("attackResult").innerHTML = attackResult(attackForce(2, 10, 10), totalDamage(attackForce(2, 10, 10), defenseForce(1, 10, 10, 1.5)), 2, 1)
 
-//calculates how much return damage is dealt by the defender
+// calculates how much return damage is dealt by the defender
 function defenseResult(defenseForce, totalDamage, defenderDefense) {
     var finalDefenseResult = round((defenseForce / totalDamage) * defender.defense * accelerator)
     
     return finalDefenseResult;
 }
+document.getElementById("defenseResult").innerHTML = defenseResult
+
